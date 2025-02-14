22867b78d182c38726ec1bea4d26afbc0cbd86cd
noiqum <onurcoskun99@gmail.com>
2025-02-14 04:23:41 +0300

    theme style update

---
+++ src/styles/theme.ts
@@ -0,0 +1,18 @@
+// themes.ts
+const lightTheme = {
+    primary: '#b59469',   // Main color
+    secondary: '#FBF8F4', // Secondary color
+    accent: '#A45851',     // Accent color
+    background: '#FFFFFF', // Light background
+    text: '#000000',       // Light text color
+};
+
+const darkTheme = {
+    primary: '#A45851',   // Darker variant for main color
+    secondary: '#3C3C3C', // Dark background
+    accent: '#b59469',     // Lighter variant for accent color
+    background: '#000000', // Dark background
+    text: '#FFFFFF',       // Dark text color
+};
+
+export { lightTheme, darkTheme };
